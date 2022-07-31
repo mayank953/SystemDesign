06f8b9cc046e6f838597ac6aa12cd5d70ecee79c
Mayank <mayank953@gmail.com>
2022-07-31 13:39:44 +0530

    Inititalize

---
+++ src/main/java/SnakeAndLadder/Main.java
@@ -0,0 +1,8 @@
+package SnakeAndLadder;
+
+public class Main {
+
+    public static void main(String args[]) {
+        System.out.println("Hello from Snakes and Ladder ");
+    }
+}
---
+++ src/main/java/elevatorDesign/Main.java
@@ -0,0 +1,10 @@
+package elevatorDesign;
+
+public class Main {
+    public static void main(String args[]) {
+
+        System.out.println("Hello from Elevator");
+
+
+    }
+}
